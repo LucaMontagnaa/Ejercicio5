6814cfbfcad8b6ff298fafcc8635fb7046754f88
LucaMontagnaa <101721205+LucaMontagnaa@users.noreply.github.com>
2022-04-10 20:24:08 -0300

    Initial commit

---
+++ Assets/Scripts/Ejercicio5.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    int num3;
+
+    void Start()
+    {
+        if (num1 == 0 || num2 == 0)
+        {
+            Debug.Log("No se puede dividir por 0");
+        }
+
+        else
+        {
+            num3 = num1 / num2;
+            Debug.Log($"{num1}/{num2} = {num3}");
+        }
+    }
+
+    
+}
